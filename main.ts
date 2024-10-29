ac30e032dc26755619fb728ec7707b82c0dccc2f
GucioPL123 <166158045+GucioPL123@users.noreply.github.com>
2024-10-29 17:41:06 +0100

    Sound ON/OFF

--- main.ts
+++ main.ts
@@ -1,11 +1,21 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 0) {
-        music.stopAllSounds()
-        basic.clearScreen()
-    } else if (receivedNumber == 1) {
+    if (receivedNumber == 1 && ALARM == true) {
         basic.showIcon(IconNames.Angry)
         music.play(music.tonePlayable(523, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
         music.play(music.tonePlayable(349, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    } else if (receivedNumber == 1 && ALARM == false) {
+        basic.showIcon(IconNames.Angry)
+    } else {
+        basic.clearScreen()
+        music.stopAllSounds()
     }
 })
+input.onButtonPressed(Button.A, function () {
+    ALARM = false
+})
+input.onButtonPressed(Button.B, function () {
+    ALARM = true
+})
+let ALARM = false
 radio.setGroup(1)
+ALARM = false
